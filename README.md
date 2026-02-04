dff1bbc4873e53f41d40d8fda2b92594a4330445
Eric Brousseau <emb417@gmail.com>
2026-02-04 10:52:15 -0800

    updated to call new api uri and use dynamic versioning

--- README.md
+++ README.md
@@ -1,111 +1,105 @@
 # vpc-get-high-scores-image
 
-This tool will pull data and create high score images for PinUP Popper to display when a mapped button is pressed.  This brings the VPC High Scores onto your cab.  It can update on startup of your cab and when entering and exiting the table.
+This tool will pull data and create high score images for PinUP Popper to display when a mapped button is pressed. This brings the VPC High Scores onto your cab. It can update on startup of your cab and when entering and exiting the table.
 
-**CAUTION:  Please make sure you backup your PinUP Popper DB before making these changes!!!**
+**CAUTION: Please make sure you backup your PinUP Popper DB before making these changes!!!**
 
-**Adding VPS Id to table(s) in PinUP Popper**:
+## **Adding VPS Id to table(s) in PinUP Popper**
 
-1. This tool requires the use of the VPS ID from http://virtual-pinball-spreadsheet.web.app (hereby known as VPS).  The easiest way to add the VPS Id to a table is through the import process via PinUP Popper.
+1. This tool requires the use of the VPS ID from <https://virtualpinballspreadsheet.github.io/> (hereby known as VPS). The easiest way to add the VPS Id to a table is through the import process via PinUP Popper.
 
-You can see the VPS Id for a table by using the instruction in the image below:
-![image](https://user-images.githubusercontent.com/1703672/162555475-e02046a8-45ab-45d7-8845-6ae3411d9033.png)
+You can see the VPS Id for a table in the image below:
+![image](vps_id_location.png)
 
+**1a.** Download an updated version of the puplookup.csv from VPS to the PinUPSystem folder from: <https://virtualpinballspreadsheet.github.io/export>
 
-  **1a.** Download an updated version of the puplookup.csv from VPS to the PinUPSystem folder:
-    ![image](https://user-images.githubusercontent.com/1703672/158044621-02b8ebba-1be9-4a75-b8a7-4e6cdfb711b9.png)
+**1b.** You will now need to open up the puplookup.csv file and change the `VPS-ID` column name in the header row to the field name you want to import the VPS Id into the PinUP Popper DB. - Example: I want to store the VPS ID into the Custom3 field in Game Manager. I will change the last field to `Custom3`
 
-  **1b.** You will now need to open up the puplookup.csv file and change the last column name in the header row to the field name you want to import the VPS Id into the PinUP Popper DB.
-    - Example:  I want to store the VPS ID into the Custom3 field in Game Manager.  I will change the last field to `Custom3`
- 
- **1c.** You now need to import the puplookup.csv into PinUP Popper.  This is done by Pinup Popper Setup > Advanced Setup > Import CSV.   When the import completes, the VPS ID will now be imported into the field you renamed in the header, when you hit import in Game Manager.
+**1c.** You now need to import the puplookup.csv into PinUP Popper. This is done by Pinup Popper Setup > Advanced Setup > Import CSV. When the import completes, the VPS ID will now be imported into the field you renamed in the header, when you hit import in Game Manager.
 
- **1d.** You now need to re-import data for each table so it picks up the VPS ID into the field you determined in the puplookup.csv header.
+**1d.** You now need to re-import data for each table so it picks up the VPS ID into the field you determined in the puplookup.csv header.
 
+## **Setup the batch file to run on Windows startup**
 
-**Setup the batch file to run on Windows startup**
+1. Using <https://github.com/emb417/vpc-get-high-scores-image/releases>, download the following files to the `C:\Pinball\PinUPSystem\PinupPopper\LAUNCH` folder (**IMPORTANT: BE AWARE THE LAUNCH FOLDER MIGHT BE IN A DIFFERENT LOCATION IF YOU HAVE USED BALLER INSTALLER**).
 
-2. Using https://github.com/ericfaris/vpc-get-high-scores-image/releases, download the following files to the `C:\Pinball\PinUPSystem\PinupPopper\LAUNCH` folder (**IMPORTANT: BE AWARE THE LAUNCH FOLDER MIGHT BE IN A DIFFERENT LOCATION IF YOU HAVE USED BALLER INSTALLER**).
-    - ![image](https://user-images.githubusercontent.com/1703672/158728829-66d670e2-8521-40ed-bdd4-968cdc835c18.png)   
-    
+2. Open `POPMENU_GetHighScoresForAllTables.bat` and edit line 3 to conform to you folder structure.
+   - "Full path of vpc-get-high-scores-image.exe" "Update High Scores For All Tables" "VPS Id" "VPS Id Field Name" "Full path to PinUP System folder" "Full path to your media folder for Other2" "max number of rows to display in score list" "suffix to add to filename in case you want to use the PinUP Popper ability to have multiple files in a particular media folder"
+   - Example: `"%_curloc%\vpc-get-high-scores-image.exe" "True" "" "CUSTOM3" "%_ParentFolderName%" "%_ParentFolderName%\POPmedia\Visual Pinball X\Other2" "10" ""`
+     - **You will need change the CUSTOM3 field above to match the field you have chosen to house the VPS Id in Step 1 - 1d**
 
-3. Open `POPMENU_GetHighScoresForAllTables.bat` and edit line 3 to conform to you folder structure.
-    - "Full path of vpc-get-high-scores-image.exe" "Update High Scores For All Tables" "VPS Id" "VPS Id Field Name" "Full path to PinUP System folder" "Full path to your media folder for Other2" "max number of rows to display in score list" "suffix to add to filename in case you want to use the PinUP Popper ability to have multiple files in a particular media folder"
-    - Example: `"%_curloc%\vpc-get-high-scores-image.exe" "True" "" "CUSTOM3" "%_ParentFolderName%" "%_ParentFolderName%\POPmedia\Visual Pinball X\Other2" "10" ""`
-        - **You will need change the CUSTOM3 field above to match the field you have chosen to house the VPS Id in Step 1 - 1d**
+3. Create a shortcut to `POPMENU_GetHighScoresForAllTables.bat`
 
-4. Create a shortcut to `POPMENU_GetHighScoresForAllTables.bat`
+4. Copy the shortcut to your Windows startup folder
 
-5. Copy the shortcut to your Windows startup folder
+## **Setup scripts to run the vpc-get-high-scores-image.exe on Launch and Close of the table**
 
-**Setup scripts to run the vpc-get-high-scores-image.exe on Launch and Close of the table**
+1. Pinup Popper Setup > Popper Setup Tab > Emulators > Visual Pinball X > Launch Setup Tab
+   - Paste the following at the end of 1. **Launch Script** and 2. **Close Script**:
+     - `START /min "" "[STARTDIR]LAUNCH\vpc-get-high-scores-image.exe" "False" "[CUSTOM3]" "CUSTOM3" "C:\Pinball\PinUPSystem" "C:\Pinball\PinUPSystem\POPMedia\Visual Pinball X\Other2" "10" ""`
+       - **You will need change the CUSTOM3 fields above to match the field you have chosen to house the VPS Id in Step 1 - 1d**
+2. Save and Close
 
-6. Pinup Popper Setup > Popper Setup Tab > Emulators > Visual Pinball X > Launch Setup Tab
-    - Paste the following at the end of 1. **Launch Script** and 2. **Close Script**:
-        - `START /min "" "[STARTDIR]LAUNCH\vpc-get-high-scores-image.exe" "False" "[CUSTOM3]" "CUSTOM3" "C:\Pinball\PinUPSystem" "C:\Pinball\PinUPSystem\POPMedia\Visual Pinball X\Other2" "10" ""`
-            - **You will need change the CUSTOM3 fields above to match the field you have chosen to house the VPS Id in Step 1 - 1d**
-        
-7. Save and Close
+## **Enable Display to Show Other2**
 
-**Enable Display to Show Other2**
+1. Pinup Popper Setup > Popper Setup Tab > GlobalConfig button > Displays tab
+   - Set `Other 2` = `Active and Hidden`
+2. Save
 
-8. Pinup Popper Setup > Popper Setup Tab > GlobalConfig button > Displays tab
-    - Set `Other 2` = `Active and Hidden`
-    
-9. Save
+3. Pinup Popper Setup > Popper Setup Tab > Screens / Themes button
 
-10. Pinup Popper Setup > Popper Setup Tab > Screens / Themes button
+4. In Pup Pack Editor
+   - Change Mode field of `Other` to `ForcePop`
+5. Click "Save PuP-Pack" button
 
-11. In Pup Pack Editor
-    - Change Mode field of `Other` to `ForcePop`
-    
-12. Click "Save PuP-Pack" button
+## **Configure and Place the Other Display**
 
-**Configure and Place the Other Display**
+1. On the same "PuP Editor" screen, click "Configure Display/Locations" button
 
-13. On the same "PuP Editor" screen, click "Configure Display/Locations" button
+2. On the "PinUP Player DIsplays" window, click on `Other2` in the "Select Screen" list
 
-14. On the "PinUP Player DIsplays" window, click on `Other2` in the "Select Screen" list
+3. Adjust this display to your liking. This will be the display for the high scores.
+   - Suggestions:
+     - Rotation: `270`
+     - Width: `2000`
+     - Height: `1800`
+     - Default State: `off`
+4. Click "Save Settings" button
 
-15. Adjust this display to your liking.  This will be the display for the high scores.
-    - Suggestions:
-        - Rotation: `270`
-        - Width: `2000`
-        - Height: `1800`
-        - Default State: `off`
-        
-16. Click "Save Settings" button
+5. Close "PuP Pack Editor" window
 
-17. Close "PuP Pack Editor" window
+## **Configure key in PinUP Popper to display Other2 (the high score image)**
 
-**Configure key in PinUP Popper to display Other2 (the high score image)**
+1. On "PinUP Popper Setup" window, click "Controller Setup" button
 
-18. On "PinUP Popper Setup" window, click "Controller Setup" button
+2. Assign key press to `Show Other` entry
 
-19. Assign key press to `Show Other` entry
+3. Click "Close" button
 
-20. Click "Close" button
+4. On "PinUP Popper Setup" window, click "Exit Setup" button
 
-21. On "PinUP Popper Setup" window, click "Exit Setup" button
+## **Test Getting High Scores**
 
-**Test Getting High Scores**
+1. Navigate to the C:\Pinball\PinUPSystem\PinupPopper\LAUNCH folder
 
-22. Navigate to the C:\Pinball\PinUPSystem\PinupPopper\LAUNCH folder
+2. Run `POPMENU_GetHighScoresForAllTables.bat`
+   You should see a command window start executing and pulling down the images...
+   You should also be able to check your `C:\Pinball\PinUPSystem\POPMedia\Visual Pinball X\Other2` folder to see the high score images being created.
 
-23. Run `POPMENU_GetHighScoresForAllTables.bat` 
-    You should see a command window start executing and pulling down the images...
-    You should also be able to check your `C:\Pinball\PinUPSystem\POPMedia\Visual Pinball X\Other2` folder to see the high score images being created.
+3. Run PinUP Popper
 
-24. Run PinUP Popper
+4. Navigate to a table
 
-25. Navigate to a table
+5. Press button to display Other2 that you set in step #19.
 
-26. Press button to display Other2 that you set in step #19.
+## **Refreshing All High Scores While in PinUP Popper**
 
-**Refreshing All High Scores While in PinUP Popper**
+1. Get to the Operator Menu in PinUP Popper
 
-27.  Get to the Operator Menu in PinUP Popper
+2. Scroll to Custom Scripts
 
-28.  Scroll to Custom Scripts
+3. Press GetHighScoresForAllTables
 
-29.  Press GetHighScoresForAllTables
+## TODOs
+
+- Update Readme, possibly fuzzy name match when vps id doesn't exist
--- src/createHighScoreImage.py
+++ src/createHighScoreImage.py
@@ -12,123 +12,164 @@ from logging.handlers import RotatingFileHandler
 import time
 import requests
 from requests.adapters import HTTPAdapter
-from urllib3.util.retry import Retry;
+from urllib3.util.retry import Retry
 
-apiBaseUri = "https://virtualpinballchat.com:8443"
+apiBaseUri = "https://virtualpinballchat.com"
 scoreUri = apiBaseUri + "/vpc/api/v1/scoresByVpsId?vpsId="
 convertUri = apiBaseUri + "/vpc/api/v1/convert"
 headers = {
-  'Authorization': 'Bearer ODYwMzEwODgxNTc3NDY3OTA0.YN5Y8Q.0P5EwvlXHG6YOtNfkWKt_xOFTtc',
-  'Content-Type': 'application/json'
+    "Content-Type": "application/json",
 }
+
 
 def set_file_last_modified(file_path, dt):
     dt_epoch = dt.timestamp()
     os.utime(file_path, (dt_epoch, dt_epoch))
 
+
 def log_setup():
-    logName = 'vpc-get-high-scores-image.log'
+    logName = "vpc-get-high-scores-image.log"
     log_handler = RotatingFileHandler(logName, maxBytes=10000000, backupCount=3)
     formatter = logging.Formatter(
-        '%(asctime)s | %(levelname)s : %(message)s',
-        '%b %d %H:%M:%S')
+        "%(asctime)s | %(levelname)s : %(message)s", "%b %d %H:%M:%S"
+    )
     formatter.converter = time.gmtime  # if you want UTC time
     log_handler.setFormatter(formatter)
     logger = logging.getLogger()
     logger.addHandler(log_handler)
     logger.setLevel(logging.INFO)
 
+
 def createImage(scoreList, mediaPath, gameName, fileNameSuffix):
-    payload = json.dumps({
-        "text": scoreList
-    })
+    payload = json.dumps({"text": scoreList})
 
     session = requests.Session()
     retry = Retry(connect=3, backoff_factor=1)
     adapter = HTTPAdapter(max_retries=retry)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
     res = session.request("POST", convertUri, headers=headers, data=payload)
 
-    imageString = res.text.replace('data:image/png;base64,', '')
+    imageString = res.text.replace("data:image/png;base64,", "")
     fullPath = mediaPath + "\\" + gameName + fileNameSuffix + ".png"
 
-    logging.info(f'fullPath: {fullPath}')
+    logging.info(f"fullPath: {fullPath}")
 
     if os.path.exists(fullPath):
-        logging.info(f'removing: {fullPath}')
+        logging.info(f"removing: {fullPath}")
         os.remove(fullPath)
-    
+
     with open(fullPath, "wb") as fh:
-        logging.info(f'creating: {fullPath}')
+        logging.info(f"creating: {fullPath}")
         fh.write(base64.b64decode(imageString))
 
     atime, mtime = os.stat(fullPath).st_atime, os.stat(fullPath).st_mtime
     os.utime(fullPath, (atime, mtime))
 
-      
+
 def fetchHighScoreImage(vpsId, fieldNames, numRows, mediaPath):
-  logging.info(f'\n\n----- fetchHighScoreImage Start')
-  logging.info(f'vpsId: {vpsId}, numRows: {numRows}, mediaPath: {mediaPath}')
+    logging.info(f"\n\n----- fetchHighScoreImage Start")
+    logging.info(f"vpsId: {vpsId}, numRows: {numRows}, mediaPath: {mediaPath}")
 
-  table = getTableFromPopperDB(vpsId, dbPath)
-  gameName = table[fieldNames.index("GameName")]
-  gameDisplay = table[fieldNames.index("GameDisplay")]
-  authorName = table[fieldNames.index("Author")]
-  scoreList = "VPS Id (" + vpsIdField + "): " + vpsId + "\n"
-  scoreList += "Screen Name: " + gameDisplay + "\n"
-  scoreList += "Author: " + authorName + "\n\n"
- 
-  scoreFullUri = scoreUri + urllib.parse.quote(vpsId)
+    table = getTableFromPopperDB(vpsId, dbPath)
+    gameName = table[fieldNames.index("GameName")]
+    gameDisplay = table[fieldNames.index("GameDisplay")]
+    authorName = table[fieldNames.index("Author")]
+    scoreList = "VPS Id (" + vpsIdField + "): " + vpsId + "\n"
+    scoreList += "Screen Name: " + gameDisplay + "\n"
+    scoreList += "Author: " + authorName + "\n\n"
 
-  session = requests.Session()
-  retry = Retry(connect=3, backoff_factor=1)
-  adapter = HTTPAdapter(max_retries=retry)
-  session.mount('http://', adapter)
-  session.mount('https://', adapter)
+    scoreFullUri = scoreUri + urllib.parse.quote(vpsId)
 
-  tables = (session.get(scoreFullUri)).json()
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=1)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
 
-  if len(tables) > 0:
-    limitedList = tables[0]['scores'][:numRows]
+    tables = (session.get(scoreFullUri)).json()
 
-    if len(tables[0]['scores']) > 0:
+    if len(tables) > 0:
+        limitedList = tables[0]["scores"][:numRows]
 
-      try:
-        hasDataErrors = False
-        rankMaxLength = len(str("Rank"))
-        userNameMaxLen = max(len(x['user']['username']) for x in limitedList)
-        scoreMaxLen = max(max(len(str("{:,}".format(int(x['score'])))) for x in limitedList), len("Score"))
-        versionMaxLen = max(max(len(x['versionNumber']) for x in limitedList), len("Version"))
-        postedMaxLen = max(max(len(x['posted']) for x in limitedList), len("Posted"))
-      except Exception as err:
-        hasDataErrors = True
-        scoreList += "This table has invalid/bad data in the VPC High Score DB.  Please contact @ericfaris.\n\n"
+        if len(tables[0]["scores"]) > 0:
 
-      if hasDataErrors == False:
-        scoreList += "Rank".ljust(rankMaxLength) + "  " + "User".ljust(userNameMaxLen) + "    " + "Score".ljust(scoreMaxLen) + "    " + "Version".ljust(versionMaxLen)  + "    " + "Posted" + '\n'       
-        scoreList += "".ljust(rankMaxLength, "-") + "  " + "".ljust(userNameMaxLen, "-") + "    " + "".rjust(scoreMaxLen, "-") + "    " + "".ljust(versionMaxLen, "-") + "    " + "".ljust(postedMaxLen, "-") + '\n'       
+            try:
+                hasDataErrors = False
+                rankMaxLength = len(str("Rank"))
+                userNameMaxLen = max(len(x["user"]["username"]) for x in limitedList)
+                scoreMaxLen = max(
+                    max(len(str("{:,}".format(int(x["score"])))) for x in limitedList),
+                    len("Score"),
+                )
+                versionMaxLen = max(
+                    max(len(x["versionNumber"]) for x in limitedList), len("Version")
+                )
+                postedMaxLen = max(
+                    max(len(x["posted"]) for x in limitedList), len("Posted")
+                )
+            except Exception as err:
+                hasDataErrors = True
+                scoreList += "This table has invalid/bad data in the VPC High Score DB.  Please contact @emb417.\n\n"
 
-        i = 1
+            if hasDataErrors == False:
+                scoreList += (
+                    "Rank".ljust(rankMaxLength)
+                    + "  "
+                    + "User".ljust(userNameMaxLen)
+                    + "    "
+                    + "Score".ljust(scoreMaxLen)
+                    + "    "
+                    + "Version".ljust(versionMaxLen)
+                    + "    "
+                    + "Posted"
+                    + "\n"
+                )
+                scoreList += (
+                    "".ljust(rankMaxLength, "-")
+                    + "  "
+                    + "".ljust(userNameMaxLen, "-")
+                    + "    "
+                    + "".rjust(scoreMaxLen, "-")
+                    + "    "
+                    + "".ljust(versionMaxLen, "-")
+                    + "    "
+                    + "".ljust(postedMaxLen, "-")
+                    + "\n"
+                )
 
-        numRows = min(numRows, len(tables[0]['scores']) ) 
+                i = 1
 
-        for score in limitedList:
-          if score.get('user'):
-            scoreList += str(i).rjust(rankMaxLength) + "  " + score['user']['username'].ljust(userNameMaxLen) + "    " + str("{:,}".format(int(score['score']))).rjust(scoreMaxLen) + "    " + score['versionNumber'].ljust(versionMaxLen) + "    " + score['posted'] + '\n'       
-            i = i + 1
+                numRows = min(numRows, len(tables[0]["scores"]))
+
+                for score in limitedList:
+                    if score.get("user"):
+                        scoreList += (
+                            str(i).rjust(rankMaxLength)
+                            + "  "
+                            + score["user"]["username"].ljust(userNameMaxLen)
+                            + "    "
+                            + str("{:,}".format(int(score["score"]))).rjust(scoreMaxLen)
+                            + "    "
+                            + score["versionNumber"].ljust(versionMaxLen)
+                            + "    "
+                            + score["posted"]
+                            + "\n"
+                        )
+                        i = i + 1
+        else:
+            scoreList += "No scores have been posted for this table.\n\n"
     else:
-      scoreList += "No scores have been posted for this table.\n\n"
-  else:
-    scoreList += "This table DOES NOT exist in the VPC High Score Corner.\nPlease contact @High Score Corner Mod to add this \ntable in the high-score-corner channel on Discord.\nPlease send the VPS ID in the message.\n\n"
+        scoreList += "This table DOES NOT exist in the VPC High Score Corner.\nPlease contact @High Score Corner Mod to add this \ntable in the high-score-corner channel on Discord.\nPlease send the VPS ID in the message.\n\n"
 
-  scoreList += "\nupdated: " +  datetime.now().strftime("%m/%d/%Y %H:%M:%S")
-  print(scoreList + "\n\n")
-  logging.info(f'Result:\n{scoreList}')
+    scoreList += "\nupdated: " + datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+    print(scoreList + "\n\n")
+    logging.info(f"Result:\n{scoreList}")
 
-  createImage(scoreList, mediaPath, gameName, fileNameSuffix)
+    createImage(scoreList, mediaPath, gameName, fileNameSuffix)
 
-  logging.info(f'----- fetchHighScoreImage End')
+    logging.info(f"----- fetchHighScoreImage End")
+
 
 def getTableFromPopperDB(vpsId, dbPath):
     conn = sqlite3.connect(dbPath + "\\" + "PUPDatabase.db")
@@ -137,82 +178,90 @@ def getTableFromPopperDB(vpsId, dbPath):
     table = cur.fetchone()
     conn.close
     return table
-  
+
+
 def strtobool(val):
     return val.lower() in ("yes", "true", "t", "1")
 
+
 log_setup()
-logging.info('--- INSTANCE STARTED ---')
+logging.info("--- INSTANCE STARTED ---")
 
 updateAll = False
 numRows = 5
 fieldNames = []
 
 try:
-  logging.info(f'args: {sys.argv[1:]}')
+    logging.info(f"args: {sys.argv[1:]}")
 
-  if len(sys.argv) > 1:
-    logging.info('Found more than 0 arguments')
-    exeName = sys.argv[0]
-    updateAll = strtobool(sys.argv[1])
-    vpsId = sys.argv[2]
-    vpsIdField = sys.argv[3]
-    dbPath = sys.argv[4]
-    mediaPath = sys.argv[5]
-    numRows = int(sys.argv[6])
-    fileNameSuffix = sys.argv[7]
-    logging.info(f'exeName: {exeName}, updateAll: {updateAll}, vpsId: {vpsId}, vpsIdField: {vpsIdField}, dbPath: {dbPath}, mediaPath: {mediaPath}, numRows: {numRows}, fileNameSuffix: ${fileNameSuffix}')
-  else:
-    logging.info('Found 0 arguments. Using default arguments for debugging')
-    updateAll = False
-    vpsId = "NTissEZP"
-    vpsIdField = "CUSTOM3"
-    dbPath = "c:\\temp"
-    mediaPath = "c:\\temp"
-    numRows = 5
-    fileNameSuffix = ""
+    if len(sys.argv) > 1:
+        logging.info("Found more than 0 arguments")
+        exeName = sys.argv[0]
+        updateAll = strtobool(sys.argv[1])
+        vpsId = sys.argv[2]
+        vpsIdField = sys.argv[3]
+        dbPath = sys.argv[4]
+        mediaPath = sys.argv[5]
+        numRows = int(sys.argv[6])
+        fileNameSuffix = sys.argv[7]
+        logging.info(
+            f"exeName: {exeName}, updateAll: {updateAll}, vpsId: {vpsId}, vpsIdField: {vpsIdField}, dbPath: {dbPath}, mediaPath: {mediaPath}, numRows: {numRows}, fileNameSuffix: ${fileNameSuffix}"
+        )
+    else:
+        logging.info("Found 0 arguments. Using default arguments for debugging")
+        updateAll = False
+        vpsId = "NTissEZP"
+        vpsIdField = "CUSTOM3"
+        dbPath = "c:\\temp"
+        mediaPath = "c:\\temp"
+        numRows = 5
+        fileNameSuffix = ""
 
-  ## fetching all tables
-  conn = sqlite3.connect(dbPath + "\\" + "PUPDatabase.db")
-  cur = conn.cursor()
-  cur.execute("SELECT * FROM 'Games' WHERE EMUID = 1 ORDER BY GameDisplay")
-  fieldNames = [description[0] for description in cur.description]
-  rows = cur.fetchall()
-  conn.close
+    ## fetching all tables
+    conn = sqlite3.connect(dbPath + "\\" + "PUPDatabase.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM 'Games' WHERE EMUID = 1 ORDER BY GameDisplay")
+    fieldNames = [description[0] for description in cur.description]
+    rows = cur.fetchall()
+    conn.close
 
-  if updateAll:
-    logging.info(f'Starting to update all tables')
-    logging.info(f'Found {str(len(rows))} tables')
-    for row in rows:
-        gameName = row[fieldNames.index("GameName")]
-        gameDisplay = row[fieldNames.index("GameDisplay")]
-        authorName = row[fieldNames.index("Author")]
-        vpsId = row[fieldNames.index(vpsIdField)]
-        if vpsId:
-          fetchHighScoreImage(vpsId, fieldNames, numRows, mediaPath)
-        else:
-          if vpsId:
-            scoreList = "VPS Id: " + vpsId + "\n"
-          else:
-            scoreList = "VPS Id: \n"  
-          if gameDisplay:
-            scoreList = "Table: " + gameDisplay + "\n"
-          else:
-            scoreList = "Table: \n"  
-          if authorName:
-            scoreList += "Author: " + authorName + "\n\n"
-          else:
-            scoreList += "Author: \n\n"
-          scoreList += "VPS Id not found.  Double the VPS Id field in PinUP Popper.\n\n"
-          scoreList += "\nupdated: " +  datetime.now().strftime("%m/%d/%Y %H:%M:%S")
-          print(scoreList + "\n\n")
-          logging.info(f'Result:\n{scoreList}')
-          createImage(scoreList, mediaPath, gameName, fileNameSuffix)
-    logging.info(f'Finished updating all tables')
-  else:
-    logging.info(f'Starting to update 1 table: ' + vpsId)
-    fetchHighScoreImage(vpsId, fieldNames, numRows, mediaPath)
+    if updateAll:
+        logging.info(f"Starting to update all tables")
+        logging.info(f"Found {str(len(rows))} tables")
+        for row in rows:
+            gameName = row[fieldNames.index("GameName")]
+            gameDisplay = row[fieldNames.index("GameDisplay")]
+            authorName = row[fieldNames.index("Author")]
+            vpsId = row[fieldNames.index(vpsIdField)]
+            if vpsId:
+                fetchHighScoreImage(vpsId, fieldNames, numRows, mediaPath)
+            else:
+                if vpsId:
+                    scoreList = "VPS Id: " + vpsId + "\n"
+                else:
+                    scoreList = "VPS Id: \n"
+                if gameDisplay:
+                    scoreList = "Table: " + gameDisplay + "\n"
+                else:
+                    scoreList = "Table: \n"
+                if authorName:
+                    scoreList += "Author: " + authorName + "\n\n"
+                else:
+                    scoreList += "Author: \n\n"
+                scoreList += (
+                    "VPS Id not found.  Double the VPS Id field in PinUP Popper.\n\n"
+                )
+                scoreList += "\nupdated: " + datetime.now().strftime(
+                    "%m/%d/%Y %H:%M:%S"
+                )
+                print(scoreList + "\n\n")
+                logging.info(f"Result:\n{scoreList}")
+                createImage(scoreList, mediaPath, gameName, fileNameSuffix)
+        logging.info(f"Finished updating all tables")
+    else:
+        logging.info(f"Starting to update 1 table: " + vpsId)
+        fetchHighScoreImage(vpsId, fieldNames, numRows, mediaPath)
 except Exception as err:
-  logging.exception(err)
+    logging.exception(err)
 
-logging.info('--- INSTANCE STOPPED ---\n\n')
+logging.info("--- INSTANCE STOPPED ---\n\n")
